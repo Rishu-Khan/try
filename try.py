6060674994510926e74e01040559b57aef7dd0f1
Rishu-Khan <88790029+Rishu-Khan@users.noreply.github.com>
2021-08-14 08:37:29 +0530

    Update try.py

--- try.py
+++ try.py
@@ -208,9 +208,9 @@ def menu():
   os.system("clear")
   print logo
   print ('\033[1;91m╔═════════════•ೋೋ•══════════════╗')
-  print ('\033[0;91m  [•]Nama : 'm+nm)
-  print ('\033[0;91m  [•]Akun ID : '+id)
-  print ('\033[0;91m  [•]Date of birth : '+tl)
+  print ('  Nama : 'm+nm)
+  print ('    Id : '+id)
+  print ('  Date of birth : '+tl)
   print ('\033[1;91m╚═════════════•ೋೋ•══════════════╝')
   
   print ('\033[1;91m╔══════════•ೋೋ•═══════════╗')
